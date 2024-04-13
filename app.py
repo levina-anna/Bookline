039ca963841c5296dcb74e20e25c293dd6f9506c
anna-levina <anna_levina@internet.ru>
2024-04-13 17:32:28 +0300

    Добавлены стили для графа

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -7,10 +8,23 @@ def index():
     return render_template('index.html')
 
 
-# Эндпоинт для отображения графика
 @app.route('/graph')
-def get_data():
-    return render_template('graph.html')
+def graph():
+    api_url = 'http://127.0.0.1:8000/products_and_categories'
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        data = response.json()
+    except requests.RequestException as e:
+        return str(e), 500
+
+    category_counts = {}
+    for entry in data:
+        category = entry['category_name']
+        category_counts[category] = category_counts.get(category, 0) + 1
+
+    return render_template('graph.html', categories=category_counts)
 
 
 if __name__ == '__main__':
